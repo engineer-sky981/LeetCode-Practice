13065938f7a1abb24504ecc063b6f92fb11f52ff
SKY <engineer.sky981@gmail.com>
2026-06-17 00:21:02 +0530

    Time: 0 ms (100%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+         std::string result = "";
+        
+        for (char c : s) {
+            if (c == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            } else if (c == '#') {
+                result += result;
+            } else if (c == '%') {
+                std::reverse(result.begin(), result.end());
+            } else {
+                result.push_back(c);
+            }
+        }
+        
+        return result;
+    }
+};
