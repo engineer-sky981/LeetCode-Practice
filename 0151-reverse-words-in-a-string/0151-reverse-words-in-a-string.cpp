53997dbd77ff167539206140703e4786c7ec0ae7
SKY <engineer.sky981@gmail.com>
2026-06-17 01:13:29 +0530

    Time: 0 ms (100%), Space: 9.8 MB (95.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-         vector<string> words;
-        string temp = "";
+     reverse(s.begin(), s.end());
         
-        // Step 1: Ek-ek karke words nikalna
-        for (char c : s) {
-            if (c != ' ') {
-                temp += c; // Word mein character add karo
-            } else {
-                if (!temp.empty()) {
-                    words.push_back(temp); // List mein add karo
-                    temp = ""; // Temp ko khali karo
+        int n = s.length();
+        int writeIdx = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != ' ') {
+                if (writeIdx > 0) {
+                    s[writeIdx++] = ' ';
                 }
+                
+                int start = writeIdx;
+                
+                while (i < n && s[i] != ' ') {
+                    s[writeIdx++] = s[i++];
+                }
+                
+                reverse(s.begin() + start, s.begin() + writeIdx);
             }
         }
         
-        // Loop ke baad bacha hua aakhri word add karna
-        if (!temp.empty()) {
-            words.push_back(temp);
-        }
-        
-        // Step 2: Words ko peeche se jodna
-        string result = "";
-        for (int i = words.size() - 1; i >= 0; --i) {
-            result += words[i];
-            if (i > 0) {
-                result += " "; // Beech mein single space lagana
-            }
-        }
-        
-        return result;
+        s.resize(writeIdx);
+        return s;
     }
 };
